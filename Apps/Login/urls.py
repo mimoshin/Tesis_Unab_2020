a124c42fd64886a6266bc26169ef8f49ac59cef4
Franco <f.vegamoya@gmail.com>
2020-09-02 18:47:35 -0400

    Crud basico

--- Apps/Login/urls.py
+++ Apps/Login/urls.py
@@ -5,4 +5,8 @@ urlpatterns =[
     path('',views.start,name='inicio'),
     path('ingreso',views.entry,name='ingreso'),
     path('index',views.index,name='index'),
+    path('registrar',views.register,name='registro'),
+    path('modificar/<int:pkid>',views.modify,name='modificar'),
+    path('eliminar/<int:pkid>',views.delete,name='eliminar'),
+    path('logout',views.user_logout,name='salir'),
 ]   
--- Apps/Login/views.py
+++ Apps/Login/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
 
 #:::::::::::::::::::Functions:::::::::::::::::::::
 
@@ -16,9 +16,11 @@ def start(request):
         pass
     if request.method == 'GET':
         pass
+    aux = str(request.user)
+    print(aux)
     return render(request,'start_page.html')
 
-#Login
+#Login_view
 def entry(request):
 
     if request.method == 'POST':
@@ -39,13 +41,61 @@ def entry(request):
     logout(request)#terminar cualquier inicio de sesion
     return render(request,'login/login.html')
 
-#Index_page
+#Index_page_view
 def index(request):
     if request.method == 'POST':
         pass
     if request.method == 'GET':
         pass
     return render(request,'login/index.html')
+
+#Inscription_user_view
+def register(request):
+    Users = list(User.objects.all())
+    if request.method == 'POST':
+        print("POST: register ---- RECEPCIONANDO INSCRIPCIÓN")
+        b = User(username=request.POST["username"],first_name=request.POST["first_name"],
+                last_name=request.POST["last_name"],email=request.POST["email"])
+        b.set_password(request.POST["password"])
+        b.save()
+
+    if request.method == 'GET':
+        return render(request,'login/register.html',{"users_list":Users})
+    return render(request,'login/register.html',{"users_list":Users})
+
+#Modifiy_user_view
+def modify(request,pkid):
+    #validar usuario
+    selected_user = User.objects.get(pk=pkid)
+    if request.method == 'POST':
+        print("POST: modify ---- Modificando al usuario ",selected_user.first_name)
+        if request.POST['first_name']:
+            selected_user.first_name = request.POST['first_name'] 
+        if request.POST['last_name']:
+            selected_user.last_name = request.POST['last_name']
+        if request.POST['email']:
+            selected_user.email = request.POST['email']
+        selected_user.save()
+        #Validar formulario
+        return redirect('index')
+    if request.method == 'GET':
+        print("GET: modify ---- selecionado el usuario ",selected_user.first_name)
+        return render(request,'login/modify.html',{"user":selected_user})
+
+    return render(request,'login/modify.html',{"user":selected_user})
+
+#Delete_user_view
+def delete(request,pkid):
+    selected_user = User.objects.get(pk=pkid)
+    print("elimnando")
+    selected_user.delete()
+    print("eliminado")
+    return redirect('/registrar')
+
+#logout_view
+def user_logout(request):
+    logout(request)
+    return redirect('/')
 #:::::::::::::::::::::::::::::::::::::::::::::::::
 
 
